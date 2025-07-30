4b6b8609a3a056f2e798c7ea1ec7820dd6ee5a7d
Bruno Mariz <bmarizot@gmail.com>
2025-07-30 17:09:20 -0300

    change min to 1

--- src/app/components/Selector.tsx
+++ src/app/components/Selector.tsx
@@ -3,10 +3,12 @@ import React from "react";
 type Props = {
   title: React.ReactNode;
   state: number;
+  min?: number;
+  max?: number;
   setState: (state: number) => void;
 };
 
-const Selector = ({ title, state, setState }: Props) => {
+const Selector = ({ title, state, setState, min = 1, max = 1000 }: Props) => {
   return (
     <div className="flex">
       {title}:
@@ -14,8 +16,8 @@ const Selector = ({ title, state, setState }: Props) => {
         type="number"
         value={state}
         onChange={(e) => setState(Number(e.target.value))}
-        min={0}
-        max={1000}
+        min={min}
+        max={max}
         step={1}
         className="border rounded p-2 m-2"
       />
