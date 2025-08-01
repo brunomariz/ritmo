bc14f5fbeacf0a5f94c5486004b5372b55356b0c
Bruno Mariz <bmarizot@gmail.com>
2025-08-01 13:35:55 -0300

    set layout to medium width

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,9 +25,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased w-screen flex justify-center`}
       >
-        {children}
+        <div className="max-w-md">{children}</div>
       </body>
     </html>
   );
